059fe36cc8cdb679a8f6eafa57db646bff3426c1
Saurav Saxena <sauravsrivastava121@gmail.com>
2024-05-14 11:24:53 +0530

    Introducing Microservices and some changes

--- order-services/src/main/java/com/codeSake/orderservices/controllers/OrderController.java
+++ order-services/src/main/java/com/codeSake/orderservices/controllers/OrderController.java
@@ -3,6 +3,7 @@ package com.codeSake.orderservices.controllers;
 
 import com.codeSake.orderservices.dto.OrderRequest;
 import com.codeSake.orderservices.services.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest){
 
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully!";
+    }
+
+
+    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
+        return "Oops! Something went wrong, Please order after some time.";
     }
 }
